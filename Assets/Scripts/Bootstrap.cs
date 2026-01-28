4d93d9103c23f92a3ae1e5e79cbdfd93d9327b35
Igor Goin <>
2026-01-28 21:51:02 +0200

    Initial setup; Added UniTask plugin

---
+++ Assets/Scripts/Bootstrap.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace CFD
+{
+    /// <summary>
+    /// This class is used to initialize core services and load the first scene.
+    /// </summary>
+    public class Bootstrap : MonoBehaviour
+    {
+        private void Awake()
+        {
+        }
+
+        private void Start()
+        {
+
+        }
+    }
+}
